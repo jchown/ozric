848a889c828b1e3cccef0f0a2dce9baa48b63651
Jason Chown <git@jasonchown.com>
2022-01-27 21:01:25 +0000

    Fill out the node tests

--- OzricEngineTests/NodeTests.cs
+++ OzricEngineTests/NodeTests.cs
@@ -12,12 +12,14 @@ namespace OzricEngineTests
         public void serializeRoundTripWorks()
         {
             assertSerializeRoundTripWorks(new Constant("red", ColorRGB.RED));
-            
-            assertSerializeRoundTripWorks(new Sensor("Main Sensor", "sensor-01"));
-            
+            assertSerializeRoundTripWorks(new DayPhases("work-times"));
+            assertSerializeRoundTripWorks(new IfAll("all-right"));
+            assertSerializeRoundTripWorks(new IfAny("any-left"));
             assertSerializeRoundTripWorks(new Light("Main Light", "hue-light-1"));
-            
             assertSerializeRoundTripWorks(new ModeSwitch("bot-mode"));
+            assertSerializeRoundTripWorks(new Sensor("Main Sensor", "sensor-01"));
+            assertSerializeRoundTripWorks(new SkyBrightness());
+            assertSerializeRoundTripWorks(new Switch("bot-colour", ValueType.Color));
         }
         
         private void assertSerializeRoundTripWorks(Node node1)
