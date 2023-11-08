f0af546b633a3abc40fad88a4155beefff429129
Jason Chown <git@jasonchown.com>
2023-11-08 22:01:00 +0000

    Use explicit slug

--- OzricEngine/Supervisor.cs
+++ OzricEngine/Supervisor.cs
@@ -11,7 +11,7 @@ public static class Supervisor
         var token = Environment.GetEnvironmentVariable("SUPERVISOR_TOKEN") ?? throw new Exception("SUPERVISOR_TOKEN not set");
         using var httpClient = new HttpClient();
         httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        var response = await httpClient.GetAsync("http://supervisor/addon/self/config");
+        var response = await httpClient.GetAsync("http://supervisor/addon/ozric/config");
         var json = await response.Content.ReadAsStringAsync();
         //var data = Json.Deserialize<DownloadData>(json);
         return json;
